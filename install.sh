93aa3fb026f3d979bf4f8af5463e9252a9a8ce42
rrkai <59231078+rrkai@users.noreply.github.com>
2025-12-10 01:40:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -502,13 +502,16 @@ echo -e "${cyan}${vless_reality_url}${none}"
 echo
 sleep 3
 echo "---------- 二维码 (UTF8) ----------"
+# 移除 -s 1 参数，使用默认大小以保证可读性
 qrencode -t UTF8 "$vless_reality_url_encoded"
 echo
 echo "---------- END -------------"
+echo "以上节点信息保存在 ~/_vless_reality_url_ 中"
 
 # 节点信息保存到文件中
 echo $vless_reality_url > ~/_vless_reality_url_
 echo "---------- 二维码 (UTF8) ----------" >> ~/_vless_reality_url_
+# 同样移除保存到文件中的二维码的 -s 1 参数
 qrencode -t UTF8 "$vless_reality_url_encoded" >> ~/_vless_reality_url_
 
 echo
